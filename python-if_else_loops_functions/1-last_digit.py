61606cf393d3830ce28299c80609478cdcdbc9b2
Schpser <schps.games@gmail.com>
2025-09-03 19:09:46 +0200

    Correcting zero to 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 str = "Last digit of"
 str_1 = "and is greater than 5"
-str_2 = "and is zero"
+str_2 = "and is 0"
 str_3 = "and is less than 6 and not 0"
 
 last_digit = abs(number) % 10 if number >= 0 else -(-number % 10)
