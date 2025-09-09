799848aae05862f08ecd82957c7b8b9c6fffb1df
Schpser <schps.games@gmail.com>
2025-09-09 13:20:43 +0200

    Correct pycode style

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,10 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
-    
+    roman_dict =
+    {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev_value = 0
-    
     for char in reversed(roman_string):
         value = roman_dict[char]
         if value < prev_value:
@@ -16,5 +13,4 @@ def roman_to_int(roman_string):
         else:
             total += value
         prev_value = value
-    
     return total
