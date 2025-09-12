370f1183c5c824634109abc2fed070464c72bc8d
Schpser <schps.games@gmail.com>
2025-09-12 12:11:26 +0200

    Add a case

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module 3-say_my_name
-Contains : function to print a name
+Contains function to print name
 """
 
 
@@ -13,13 +13,12 @@ def say_my_name(first_name, last_name=""):
         first_name: first name string
         last_name: last name string (optional)
 
-    Returns:
-        None.
     Raises:
         TypeError: if first_name or last_name are not strings
     """
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
+
     print(f"My name is {first_name} {last_name}")
