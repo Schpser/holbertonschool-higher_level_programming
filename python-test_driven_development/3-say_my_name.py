8fdefd3246c1e7f9baa5ca0b04d612fed360d061
Schpser <schps.games@gmail.com>
2025-09-12 11:32:06 +0200

    Checking tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
-"""Module for printing names"""
+"""
+Module 3-say_my_name
+Contains function to print name
+"""
+
 
 def say_my_name(first_name, last_name=""):
-    """Prints a name"""
-    pass
+    """
+    Prints My name is <first name> <last name>
+
+    Args:
+        first_name: first name string
+        last_name: last name string (optional)
+
+    Raises:
+        TypeError: if first_name or last_name are not strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    
+    print(f"My name is {first_name} {last_name}")
--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-"""Module for printing squares"""
+"""
+Module 4-print_square
+Contains function to print square with #
+"""
+
 
 def print_square(size):
-    """Prints a square"""
-    pass
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        return
+
+    for i in range(size):
+        print("#" * size)
