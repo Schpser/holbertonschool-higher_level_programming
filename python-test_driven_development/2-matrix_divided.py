76d447539b61173100809f96c74d10d9d7f73609
Schpser <schps.games@gmail.com>
2025-09-12 10:38:05 +0200

    Divide Matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -34,25 +34,31 @@ def matrix_divided(matrix, div):
                 msg = "matrix must be a matrix (list of lists) of integers/floats"
                 raise TypeError(msg)
 
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(isinstance(elem, (int, float)) for row in matrix
+               for elem in row):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
+    if matrix == [[]]:
+        return []
 
     new_matrix = []
     for row in matrix:
-        new_row = []
-        for element in row:
-            result = element / div
-            if abs(result) < 1e-10:
-                new_row.append(0.0)
-            else:
-                new_row.append(round(result, 2))
+        new_row = [round(elem / div, 2) for elem in row]
         new_matrix.append(new_row)
 
     return new_matrix
