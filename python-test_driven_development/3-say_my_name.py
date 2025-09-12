1f5e18e4fbc8b57a2d036be69020762e22895dd5
Schpser <schps.games@gmail.com>
2025-09-12 12:06:23 +0200

    Add arguments and check Pycodestyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module 3-say_my_name
-Contains function to print name
+Contains : function to print a name
 """
 
 
@@ -12,7 +12,10 @@ def say_my_name(first_name, last_name=""):
     Args:
         first_name: first name string
         last_name: last name string (optional)
-
+        
+    Returns:
+        None.
+    
     Raises:
         TypeError: if first_name or last_name are not strings
     """
@@ -20,5 +23,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
     print(f"My name is {first_name} {last_name}")
