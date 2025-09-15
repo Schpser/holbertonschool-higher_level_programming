f854058966d326c206cba8926df0f8ca4b4b4c11
Schpser <schps.games@gmail.com>
2025-09-15 13:16:41 +0200

    Set setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,7 +1,42 @@
 #!/usr/bin/python3
 
-# Write an empty class Square that defines a square
+"""Write an empty class Square that defines a square."""
+
 
 class Square:
-    """Empty class Square"""
-    pass
+    """Class Square with size validation"""
+
+    def __init__(self, size=0):
+        """Initialize Square with setter and getter
+
+        Args:
+            size (int, optional): size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter with validation."""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the current square area
+
+        Returns:
+            int: area of the square (size * size)
+        """
+
+        return self.__size ** 2
