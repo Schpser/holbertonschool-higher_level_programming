f3a32729688c48df572a3f18cdc7e19305bf96a3
Schpser <schps.games@gmail.com>
2025-09-15 10:43:01 +0200

    Fix pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-'''Write an empty class Square that defines a square''' 
+'''Write an empty class Square that defines a square.'''
+
 
 class Square:
     """Class Square with private instance attribute size"""
 
     def __init__(self, size):
         """Initialize Square with size
-        
+
         Args:
             size: size of the square
         """
