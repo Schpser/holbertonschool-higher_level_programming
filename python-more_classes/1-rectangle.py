88a8fa2ba72f2a1d159367494d4a83c796c4f62d
Schpser <schps.games@gmail.com>
2025-09-15 20:50:09 +0200

    Fix pycodestyle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,26 +7,26 @@ class Rectangle:
     """Create an empty class called Rectangle"""
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-        
+
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
         """
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """Get the width of the rectangle."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Set the width of the rectangle.
-        
+
         Args:
             value (int): The width value to set.
-            
+
         Raises:
             TypeError: If width is not an integer.
             ValueError: If width is less than 0.
@@ -36,19 +36,19 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Get the height of the rectangle."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Set the height of the rectangle.
-        
+
         Args:
             value (int): The height value to set.
-            
+
         Raises:
             TypeError: If height is not an integer.
             ValueError: If height is less than 0.
