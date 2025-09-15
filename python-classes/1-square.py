9c2e025f8d1b4d597e58967f30dc65bb6640d7b6
Schpser <schps.games@gmail.com>
2025-09-15 10:24:30 +0200

    Create pribate instance for Squares size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
-# Write an empty class Square that defines a square
+'''Write an empty class Square that defines a square''' 
 
 class Square:
-    """Empty class Square"""
-    pass
+    """Class Square with private instance attribute size"""
+
+    def __init__(self, size):
+        """Initialize Square with size
+        
+        Args:
+            size: size of the square
+        """
+        self.__size = size
