7db1c7d6c4589f87e652f52acd1cfba481d3edb9
Schpser <schps.games@gmail.com>
2025-09-15 10:34:34 +0200

    Define Square with a Class Square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-# Write an empty class Square that defines a square
-
+''' Create a Square instance that defines a Square '''
 class Square:
-    """Empty class Square"""
-    pass
+    """Define a Square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
