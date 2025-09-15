a186a4fceb59f4dc5a0f8262ac15ca328e3b657a
Schpser <schps.games@gmail.com>
2025-09-15 10:39:30 +0200

    Fix Argument

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Write an empty class Square that defines a square """
+"""Write an empty class Square that defines a square."""
 
 class Square:
-    """Empty class Square : Write an Empty Square class"""
+    """Empty class Square : Write an Empty Square class."""
     pass
