25ec2e21dbfb3b9ce1319365c05748b80aeb3003
Schpser <schps.games@gmail.com>
2025-09-16 10:27:12 +0200

    Add Number of Instances

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -5,6 +5,8 @@
 
 class Rectangle:
     """Create an empty class called Rectangle"""
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
@@ -14,6 +16,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -84,3 +87,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
