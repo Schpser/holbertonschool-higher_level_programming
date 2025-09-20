7dbf95e2723b9164fea80b00b76435629c5b6aea
Schpser <schps.games@gmail.com>
2025-09-20 14:59:56 +0200

    Fix pycodestyle

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module documentation"""
 
+
 class BaseGeometry:
     """Empty geometry class"""
     pass
--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module documentation"""
 
+
 class BaseGeometry:
     """Geometry class with area method"""
-    
