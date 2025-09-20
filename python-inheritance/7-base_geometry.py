fb4143e04c1065c5cac7f078fb1d6821a1111664
Schpser <schps.games@gmail.com>
2025-09-20 15:13:42 +0200

    Add area method

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-"""Module documentation"""
+"""BaseGeometry class with area method"""
+
 
 class BaseGeometry:
-    """Geometry class with validation"""
+    """A base geometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
