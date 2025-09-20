d0bfaa93b70339a7fcbfe54a13de41f208f71e0c
Schpser <schps.games@gmail.com>
2025-09-20 15:15:42 +0200

    Add documentation

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,9 +6,13 @@ class BaseGeometry:
     """A base geometry class"""
 
     def area(self):
+        """Raises an Exception that area is not implemented"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validates that value is a positive integer"""
+
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
