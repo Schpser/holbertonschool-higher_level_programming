73577b749362c6a78561e17ac42329664364c312
Schpser <schps.games@gmail.com>
2025-09-20 14:54:23 +0200

    Fix pycodestyle

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,7 +4,7 @@
 
 class MyList(list):
     """A class that inherits from list"""
-    
+
     def print_sorted(self):
         """Prints the list sorted in ascending order"""
         sorted_list = sorted(self)
