de2bef2a1d44b7baab47fb4147fdf9ab0402e0ee
Schpser <schps.games@gmail.com>
2025-09-20 15:07:23 +0200

    Fix Pycodestyle (Again)

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -4,7 +4,7 @@
 
 class BaseGeometry:
     """A base geometry class"""
-    
+
     def area(self):
         """Raises an Exception that area is not implemented"""
         raise Exception("area() is not implemented")
