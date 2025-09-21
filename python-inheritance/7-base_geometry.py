ae06378eb57ea04fb619bb11392eed6bfd7763b5
Schpser <schps.games@gmail.com>
2025-09-21 13:22:44 +0200

    Pycodestyle + lasts tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,14 +4,14 @@
 
 class BaseGeometry:
     """A base geometry class"""
-    
+
     def area(self):
         """Raises an Exception that area is not implemented"""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Validates that value is a positive integer"""
-        if type(value) is not int:  # Change ici !
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
