53068bb1023e7000613143251eb81e8d38937c15
Schpser <schps.games@gmail.com>
2025-09-21 19:17:42 +0200

    Write a class Square

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-"""Module documentation"""
-Rectangle = __import__(9-rectangle).Rectangle
+"""Write a class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
-    """Square class with custom string representation"""
+    """Square class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
