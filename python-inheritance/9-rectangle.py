17798bef16dccfa7ed029d8648b9e7e2c8802bd6
Schpser <schps.games@gmail.com>
2025-09-21 13:52:58 +0200

    Inherits from precedent file

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module documentation"""
+"""Write a class Rectangle that inherits from BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
