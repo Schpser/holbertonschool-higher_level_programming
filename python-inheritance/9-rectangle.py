a60a05c2e90244b05b6abeccbc3893f71c6deee4
Schpser <schps.games@gmail.com>
2025-09-21 13:47:53 +0200

    Write a class Rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 """Module documentation"""
-BaseGeometry = __import__(7-base_geometry).BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class with area and string representation"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculate and return the area of the rectangle"""
+        return self.__width * self.__height
+    
+    def __str__(self):
+        """Return the string representation of the rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
