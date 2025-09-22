c3134599b64f9c15f6698e9c3e695f5ff0ea9e0d
Schpser <schps.games@gmail.com>
2025-09-22 11:45:56 +0200

    Python OOP

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -36,5 +36,5 @@ class Rectangle(Shape):
         return 2 * (self.width + self.height)
 
 def shape_info(obj):
-    print(f"Area: {obj.area()}")
-    print(f"Perimeter: {obj.perimeter()}")
+    print(f"Area:, {obj.area()}")
+    print(f"Perimeter:, {obj.perimeter()}")
