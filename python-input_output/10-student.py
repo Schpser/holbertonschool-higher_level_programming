d0c18680df32829c334635fced02f49c397454aa
Schpser <schps.games@gmail.com>
2025-09-29 14:55:44 +0200

    Add attrs condition

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Write a class Student that defines a student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs is None or not isinstance(attrs, list):
+            return {
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age
+            }
+
+        result = {}
+        if 'first_name' in attrs:
+            result['first_name'] = self.first_name
+        if 'last_name' in attrs:
+            result['last_name'] = self.last_name
+        if 'age' in attrs:
+            result['age'] = self.age
+
+        return result
--- python-input_output/8-my_class.py
+++ python-input_output/8-my_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" My class module
+"""
+
 class MyClass:
+    """ My class
+    """
+
     def __init__(self, name):
         self.name = name
         self.number = 0
