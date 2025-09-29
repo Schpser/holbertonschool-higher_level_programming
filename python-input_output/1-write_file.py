fde3ff6544f8b60dfad1fedf660b39f99c2128c5
Schpser <schps.games@gmail.com>
2025-09-29 12:55:37 +0200

    Write a function that writes a string to a text file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write file module"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
