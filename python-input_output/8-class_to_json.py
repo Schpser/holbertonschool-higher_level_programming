4d143dc458bc71abeb7f94c75249686211714972
Schpser <schps.games@gmail.com>
2025-09-29 14:27:09 +0200

    Fuction that returns dictionnary args

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!usr/bin/pyton3
+"""Create a function that return a dictionary description"""
+
+
+def class_to_json(obj):
+    """Returns dictionary description for JSON serialization"""
+    result = {}
+
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            result[key] = value
+
+    return result
