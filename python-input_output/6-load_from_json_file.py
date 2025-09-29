bf69ed73b4d581bf65e05803e0b5d73d1f74ee88
Schpser <schps.games@gmail.com>
2025-09-29 13:53:25 +0200

    Add a Line under Module comment

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,6 +2,7 @@
 """Create an object from JSON file"""
 import json
 
+
 def load_from_json_file(filename):
     """Creates an object to a text file using JSON"""
     with open(filename, 'r', encoding='utf-8') as f:
