f171196793bd7befa3e88d85161f2a2ba3f3600b
Schpser <schps.games@gmail.com>
2025-09-29 15:28:42 +0200

    Pasacal s Triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """Create a function for Pasacal's Triangle"""
 
+
 def pascal_triangle(n):
     """Returns Pascal's triangle of n"""
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         row = [1]
         previous_row = triangle[i-1]
 
         for j in range(1, i):
             row.append(previous_row[j-1] + previous_row[j])
-        
+
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
