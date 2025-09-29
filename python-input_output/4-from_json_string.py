156896c90d082f99bcf8970bf76922908e8178bc
Schpser <schps.games@gmail.com>
2025-09-29 13:50:09 +0200

    From string to Object, Save and Create from JSON file

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""JSON deserialization module"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns object from JSON string"""
+    return json.loads(my_str)
--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Save to JSON file module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using JSON"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
