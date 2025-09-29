00003ee53488d0d518eb36e351d45ba15be60dee
Schpser <schps.games@gmail.com>
2025-09-29 13:24:07 +0200

    Function that appends a string at the EOF

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Append write module"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string to a text file"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
