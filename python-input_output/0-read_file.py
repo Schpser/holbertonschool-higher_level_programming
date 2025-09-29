4a6d84a47ade51cca292256624fe418d136536a5
Schpser <schps.games@gmail.com>
2025-09-29 11:55:10 +0200

    Add module documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,11 +3,10 @@
 
 
 def read_file(filename=""):
-        """Reads and prints a text file's content.
+    """Reads and prints a text file's content.
     Args:
         filename (str): File path to read
     """
-
 
     with open(filename, 'r', encoding='utf-8') as f:
         content = f.read()
