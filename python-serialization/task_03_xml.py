41e9fd5464a41ecd5df88ed730edb83b8cefea33
Schpser <schps.games@gmail.com>
2025-09-30 13:26:50 +0200

    Add Codumentation

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" Module for serializing a dictionary to an XML file. """
 
 import xml.etree.ElementTree as ET
 
 
 def serialize_to_xml(dictionary, filename):
+    """Save a dictionary to an XML file. """
     root = ET.Element("data")
     for key, value in dictionary.items():
         child = ET.SubElement(root, key)
@@ -14,6 +16,7 @@ def serialize_to_xml(dictionary, filename):
 
 
 def deserialize_from_xml(filename):
+    """Load a dictionary from an XML file. """
     tree = ET.parse(filename)
     root = tree.getroot()
 
