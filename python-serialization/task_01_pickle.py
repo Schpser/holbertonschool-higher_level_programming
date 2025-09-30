8c2230793a45df1f2ce5b47ab9300df8d984418e
Schpser <schps.games@gmail.com>
2025-09-30 12:17:59 +0200

    Serialize/Deserialize using pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import pickle
+
+
+class CustomObject():
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def serialize(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}, is_student: {True if self.is_student else False}")
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except Exception:
+            return None
