aca1908612279c2d78b66ef31a5316263630bf28
Schpser <schps.games@gmail.com>
2025-10-21 20:23:56 +0200

    Back to simple version

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,16 +1,13 @@
---CREATE TABLE;
+-- CREATE TABLE;
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
---ADD ROWS;
+-- ADD ROWS;
 INSERT INTO second_table (id, name, score) VALUES 
 (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
 (4,'George', 8);
-ON DUPLICATE KEY UPDATE 
-name = VALUES(name), 
-score = VALUES(score);
