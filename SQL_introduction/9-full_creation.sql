85ed1fee64ddbadbe05e787b9e4bf3a78bf081b0
Schpser <schps.games@gmail.com>
2025-10-21 20:18:47 +0200

    Re organise the code in another way

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,3 @@
---DELETE TABLE IF IT EXISTS;
-DROP TABLE IF EXISTS second_table;
-
 --CREATE TABLE;
 CREATE TABLE second_table (
     id INT,
@@ -8,12 +5,12 @@ CREATE TABLE second_table (
     score INT
 );
 
--- DELETE OLD DATAS;
-DELETE FROM second_table WHERE id IN (1, 2, 3, 4);
-
 --ADD ROWS;
 INSERT INTO second_table (id, name, score) VALUES 
 (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
 (4,'George', 8);
+ON DUPLICATE KEY UPDATE 
+name = VALUES(name), 
+score = VALUES(score);
