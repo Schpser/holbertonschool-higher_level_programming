220b36522adfde358bdacee20850c4bbc0d1bf26
root <root@DESKTOP-O7ACI9L.localdomain>
2025-10-22 12:13:58 +0200

    Create user with conditions

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- CREATE A USER WITH CONDITIONS;
+-- PASSWORD + ALL PRIVILEGES AND SHOW IT;
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
