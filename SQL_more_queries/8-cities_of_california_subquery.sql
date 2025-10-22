d0af127f40c1c3bf51f75cf3a01e73c7dbab30a4
root <root@DESKTOP-O7ACI9L.localdomain>
2025-10-22 13:22:07 +0200

    List Cities From California, without using JOIN

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
 -- LISTS ALL CITIES FROM CALIFORNIA;
 SELECT id, name 
-FROM hbtn_0d_usa.cities 
+FROM cities 
 WHERE state_id = (
     SELECT id 
-    FROM hbtn_0d_usa.states 
+    FROM states 
     WHERE name = 'California'
 ) 
 ORDER BY id ASC;
