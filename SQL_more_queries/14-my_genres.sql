631aa1e66b4f5e9c860e197ac0cbafe44d9b6e06
root <root@DESKTOP-O7ACI9L.localdomain>
2025-10-22 17:23:52 +0200

    List all genres of show Dexter

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- LIST ALL GENRES OF SHOW 'DEXTER';
+SELECT 
+    tv_genres.name AS genre
+
+FROM tv_genres, tv_shows, tv_show_genres
+WHERE tv_genres.id = tv_show_genres.genre_id 
+AND tv_shows.id = tv_show_genres.show_id
+AND tv_shows.title = 'Dexter'
+
+ORDER BY tv_genres.name ASC;
