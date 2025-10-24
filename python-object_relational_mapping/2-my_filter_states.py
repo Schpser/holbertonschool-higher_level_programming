42cb951373d1978795f8d889eb6e84dfcbe80dc7
Schpser <schps.games@gmail.com>
2025-10-24 13:08:14 +0200

    Fix syntax

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    query = "SELECT * FROM states.id WHERE name = '{}'"\
-            "ORDER BY ASC".format(sys.argv[4])
+    query = "SELECT * FROM states WHERE name = '{}'"\
+            " ORDER BY id ASC;".format(sys.argv[4])
     cur.execute(query)
 
     results = cur.fetchall()
