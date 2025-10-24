26ef11d51efb61fdfc4253d3b9d0f3992e17fbe0
Schpser <schps.games@gmail.com>
2025-10-24 14:24:51 +0200

    Correct pycodestyle

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     Base.metadata.create_all(engine)
