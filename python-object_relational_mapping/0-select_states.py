220553b746b484b8c80d78cf9375b9da47d45daf
root <root@DESKTOP-O7ACI9L.localdomain>
2025-10-24 12:30:27 +0200

    List all rows from state table

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,16 +10,16 @@ if __name__ == "__main__":
         host="localhost",
         port=3306,
         user=sys.argv[1],
-        passwd=sys.argv[2], 
+        passwd=sys.argv[2],
         db=sys.argv[3]
     )
-    
+
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    
+
     results = cur.fetchall()
     for row in results:
         print(row)
-    
+
     cur.close()
     db.close()
