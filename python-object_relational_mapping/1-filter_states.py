3b41a3050c545a98365482dc05395a700172d522
Schpser <schps.games@gmail.com>
2025-10-24 13:54:49 +0200

    List cities from a state

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,8 +6,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if (sys.argv) != 4:
-        pass
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,7 +6,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
