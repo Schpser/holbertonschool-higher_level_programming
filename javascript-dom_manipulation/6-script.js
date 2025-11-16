db088f3db7369af682e7e2def8956194de3a7917
Schpser <schps.games@gmail.com>
2025-11-16 02:10:23 +0100

    Updates (Button & Link)

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,6 @@
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const characterElement = document.querySelector('#character');
+    characterElement.textContent = data.name;
+  });
